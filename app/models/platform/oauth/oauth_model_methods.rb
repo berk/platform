4e5cacf8fa8c99d0fbb05c65722baa05c9cd93f6
Michael Berkovich <michael@geni.com>
2011-06-30 14:33:57 -0700

    fixed tests and added merging functionality

--- app/models/platform/oauth/oauth_model_methods.rb
+++ app/models/platform/oauth/oauth_model_methods.rb
@@ -1,9 +1,9 @@
 module Platform::Oauth::OauthModelMethods
 
   def self.included(base)
-    base.has_many :applications, :class_name => "Platform::Application", :dependent=>:destroy
     base.has_many :access_tokens, :class_name => "Platform::Oauth::AccessToken", :dependent => :destroy
     base.has_many :request_tokens, :class_name => "Platform::Oauth::RequestToken", :dependent => :destroy
+    base.has_many :refresh_tokens, :class_name => "Platform::Oauth::RefreshToken", :dependent => :destroy
   end
 
   def authorized_oauth_access_tokens
@@ -13,11 +13,6 @@ module Platform::Oauth::OauthModelMethods
   def access_token_for(application)
     return unless application
     access_tokens.detect{|t| t.application_id == application.id}
-  end
-
-  def create_authorized_access_token(application)
-    return unless application
-    OauthRequestToken.create(:application => application, :user => self).try(:create_access_token)
   end
 
 end
