83a75e54105ca6ee65ca2811bef7721e87e90f10
Michael Berkovich <michael@geni.com>
2011-07-20 12:36:05 -0700

    api explorer fixes

--- public/platform/javascripts/platform.js
+++ public/platform/javascripts/platform.js
@@ -392,7 +392,37 @@ Platform.Utils = {
   displayCredits: function() {
     if (platformLightbox)
       platformLightbox.show('/platform/help/lb_credits', {width:420, height:250});
-  }
+  },
+
+	equal: function (obj1, obj2) {
+	  for(p in obj1) {
+	    if(typeof(obj2[p])=='undefined') {return false;}
+	  }
+	
+	  for(p in obj1) {
+	      if (obj1[p]) {
+	          switch(typeof(obj1[p])) {
+	                  case 'object':
+	                          if (!equal(obj1[p], x[p])) { return false }; break;
+	                  case 'function':
+	                          if (typeof(obj2[p])=='undefined' || (obj1[p].toString() != obj2[p].toString())) { return false; }; break;
+	                  default:
+	                          if (obj1[p] != obj2[p]) { return false; }
+	          }
+	      } else {
+	          if (obj2[p]) {
+	              return false;
+	          }
+	      }
+	  }
+	  
+	  for(p in obj2) {
+	      if(typeof(obj1[p])=='undefined') {return false;}
+	  }
+	  
+	  return true;
+	}
+
 
 }
 
